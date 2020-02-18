5401c17bf65600736e0ffa8ffa3481a36c6160ce
achmi <a.chmilowski@interia.pl>
2020-02-18 13:09:28 +0100

    Added SearchProduct

--- src/test/java/testtry/pages/HeaderPage.java
+++ src/test/java/testtry/pages/HeaderPage.java
@@ -1,10 +1,16 @@
 package testtry.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import testtry.drivermanage.DriverManager;
 import testtry.drivermanage.TestBase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HeaderPage extends TestBase {
 
@@ -13,6 +19,12 @@ public class HeaderPage extends TestBase {
 
     @FindBy(name = "autocomplete-search")
     public WebElement searchInput;
+
+    @FindBy(xpath = "/html/body/app-root/app-srp/div/div/div[2]/div[2]/app-srp-list/div[1]/app-product-tile-list")
+    public WebElement listOfProduct;
+
+    @FindBy(xpath = "/html/body/app-root/app-srp/div/div/div[2]/div[2]/app-srp-list/div[1]/app-product-tile-list/div/div[2]/div[2]/div[1]/app-product-tile-list-price/div/div/span[2]")
+    public WebElement listOfPrices;
 
     public HeaderPage() {
         PageFactory.initElements(DriverManager.getWebDriver(), this);
@@ -23,7 +35,23 @@ public class HeaderPage extends TestBase {
         return isDisplayed;
     }
 
-    public void inputProduct(String product) {
-        searchInput.sendKeys(product);
+    public void inputProduct(String productName) {
+        searchInput.sendKeys(productName);
+        searchInput.sendKeys(Keys.ENTER);
     }
-}
+
+    public List<String> getProducts() {
+        List<String> productList = new ArrayList<>();
+        List<WebElement> elements = listOfProduct.findElements(By.xpath("/html/body/app-root/app-srp/div/div/div[2]/div[2]/app-srp-list/div/app-product-tile-list/div/div[2]/div[1]"));
+        for (WebElement product : elements) {
+           productList.add(product.getText());
+        }
+        return productList;
+    }
+    public List<String> getPrices() {
+        List<WebElement> pricesList = listOfProduct.findElements(By.xpath("//div[contains(@class, 'price')]//span[2]"));
+        List<String> prices = pricesList.stream().map(element ->element.getText()).collect(Collectors.toList());
+        return prices;
+        }
+    }
+
--- src/test/java/testtry/test/SearchProduct.java
+++ src/test/java/testtry/test/SearchProduct.java
@@ -1,16 +1,26 @@
 package testtry.test;
-
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import testtry.drivermanage.TestBase;
 import testtry.pages.HeaderPage;
 
+import java.util.List;
+
 public class SearchProduct extends TestBase {
 
   @Test
-  public void searchProductInInput(){
+  public void searchProductInInput() throws InterruptedException {
 
-     HeaderPage searchPage = new HeaderPage();
-     searchPage.inputProduct("ciastka");
+      HeaderPage searchPage = new HeaderPage();
+      searchPage.inputProduct("czekolada");
+      List<String> productList = searchPage.getProducts();
+      Assert.assertEquals("CZEKOLADA BALANCE BEZ LAKT BEZGL 85G",productList.get(0));
+      Assert.assertEquals("FIGURKA ZAJAC Z MLECZNEJ CZEKOLADY 60G WN IDC POLONIA",productList.get(1));
+      List<String> pricesList = searchPage.getPrices();
+      Assert.assertEquals("4,33 zł",pricesList.get(0));
+      Assert.assertEquals("0,99 zł",pricesList.get(1));
   }
-
 }
+
+
+
